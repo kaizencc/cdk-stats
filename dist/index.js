09b5f25439ac46af2058bcc210ac38de59f3c0ff
Kaizen Conroy <zen.conroy@gmail.com>
2023-07-18 12:48:55 -0400

    init api

--- dist/index.js
+++ dist/index.js
@@ -6,14 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 8080; // default port to listen
 // Configure Express to use EJS
 app.set("views", path_1.default.join(__dirname, "views"));
 app.set("view engine", "ejs");
 // define a route handler for the default home page
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
     // render the index template
-    res.render("index");
+    // res.render("index");
+    return res.send('Express Typescript on Vercel');
+});
+app.get('/ping', (_req, res) => {
+    return res.send('pong 🏓');
 });
 // start the express server
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import express from "express";
+import express, { Request, Response } from 'express';
 import path from "path";
+
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 8080; // default port to listen
 
 // Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    // render the index template
-    res.render( "index" );
-} );
+app.get("/", (_req: Request, res: Response) => {
+  // render the index template
+  // res.render("index");
+  return res.send('Express Typescript on Vercel');
+});
+
+app.get('/ping', (_req: Request, res: Response) => {
+  return res.send('pong 🏓');
+});
 
 // start the express server
 app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${ port }`);
+});
